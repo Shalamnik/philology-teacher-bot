8718dada3eaa21d4f5cd741aef65c63cd19d1b5d
Шаламберидзе Никита Игоревич <nishalam@mts.ru>
2021-12-18 21:42:44 +0300

    Create send method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import telegram
 import os
+import requests
 
 
 app = Flask(__name__)
@@ -11,8 +12,17 @@ URL = os.environ['URL']
 bot = telegram.Bot(token=TOKEN)
 
 
-@app.route('/', methods=['GET', 'POST'])
+def send_message(chat_id, text):
+    method = "sendMessage"
+    url = f"https://api.telegram.org/bot{TOKEN}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
+
+
+@app.route("/", methods=["GET", "POST"])
 def receive_update():
     if request.method == "POST":
         print(request.json)
+        chat_id = request.json["message"]["chat"]["id"]
+        send_message(chat_id, "pong")
     return {"ok": True}
