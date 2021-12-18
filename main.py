858816c8878b9cd8126f24508890cc9691777bf0
Шаламберидзе Никита Игоревич <nishalam@mts.ru>
2021-12-18 23:12:12 +0300

    Update questions and answers

--- main.py
+++ main.py
@@ -26,11 +26,22 @@ def receive_update():
 
         chat_id = update.message.chat.id
         msg_id = update.message.message_id
-
         text = update.message.text.encode('utf-8').decode()
+
         if text == "/start":
-            reply_message = "Здравствуйте, я Ваш помощник по филологии: Русскому языку и литературе." \
+            reply_message = "Здравствуйте, я Ваш помощник по филологии: Русскому языку и литературе.\n" \
                             "Введите 'помощь' для просмора возможных команд."
+            bot.sendMessage(chat_id=chat_id, text=reply_message)
+        elif text == "помощь":
+            reply_message = "Для получения ДЗ введите свой класс, точку и слово 'дз'. Например, '5а.дз'.\n" \
+                            "Для получения подсказки по ДЗ введите свой класс, точку и слово 'помощь'. " \
+                            "Например, '5а.помощь'."
+            bot.sendMessage(chat_id=chat_id, text=reply_message)
+        elif "5" in text and "дз" in text:
+            reply_message = "Параграф 7, задание №3."
+            bot.sendMessage(chat_id=chat_id, text=reply_message)
+        elif "5" in text and "помощь" in text:
+            reply_message = "Ещё раз внимательно прочитайте правило в параграфе и посмотрите контрольные примеры."
             bot.sendMessage(chat_id=chat_id, text=reply_message)
         else:
             reply_message = "Команда не распознана. Попробуйте ввести ещё раз."
