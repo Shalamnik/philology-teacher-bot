8f1f46eeabd61936d7694c81c6f3cb028703ab91
Шаламберидзе Никита Игоревич <nishalam@mts.ru>
2021-12-18 22:49:32 +0300

    Update answer massage

--- main.py
+++ main.py
@@ -27,18 +27,14 @@ def receive_update():
         chat_id = update.message.chat.id
         msg_id = update.message.message_id
 
-        # Telegram understands UTF-8, so encode text for unicode compatibility
         text = update.message.text.encode('utf-8').decode()
-        # for debugging purposes only
-        print("got text message :", text)
-        # the first time you chat with the bot AKA the welcoming message
         if text == "/start":
-            # print the welcoming message
-            bot_welcome = """
-               Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-               """
-            # send the welcoming message
-            bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+            reply_message = "Здравствуйте, я Ваш помощник по филологии: Русскому языку и литературе." \
+                            "Введите 'помощь' для просмора возможных команд."
+            bot.sendMessage(chat_id=chat_id, text=reply_message)
+        else:
+            reply_message = "Команда не распознана. Попробуйте ввести ещё раз."
+            bot.sendMessage(chat_id=chat_id, text=reply_message, reply_to_message_id=msg_id)
     # return request.json
     return {"ok": True}
 
